87d973b4f63d0ebd46614566203271ca0b626b30
Gerald-Simpson <gtnsimpson@gmail.com>
2023-10-10 08:50:58 +0100

    Removed unused code from middleware that caused an error at build

--- middleware.js
+++ middleware.js
@@ -1,15 +1,12 @@
 import { NextResponse } from 'next/server';
-import { cookies } from 'next/headers';
 
 export async function middleware(request) {
   const response = NextResponse.next();
-  const mongoose = require('mongoose');
 
   // if client has cookie id, do nothing, otherwise create universally unique identifier (UUID)
   if (!request.cookies.has('id')) {
     let uuid = self.crypto.randomUUID();
     response.cookies.set('id', uuid);
-    //await mongoose.models.basket.create({ cookieId: uuid, itemDbId: [] });
   }
   return response;
 }

5d232c9dc7d154406c5ad7b5b0bd0baa6da6cde6
Gerald Simpson <gtnsimpson@gmail.com>
2024-05-29 10:46:08 +0100

    Auth0 bug fix.

--- app/admin/api/auth/[auth0]/route.tsx
+++
@@ -1,9 +0,0 @@
-import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
-
-export const GET = handleAuth({
-    async login(req: any, res: any) {
-        await handleLogin(req, res, {
-            returnTo: process.env.HOST_NAME + '/admin/successful',
-        });
-    },
-});

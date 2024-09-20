20d36b5f35d28065e8bbbcb3541bfc8ac264a536
Gerry <gtnsimpson@gmail.com>
2024-09-20 15:02:40 +0100

    Fixed type error bug.

--- app/shop/[productId]/page.tsx
+++ app/shop/[productId]/page.tsx
@@ -2,56 +2,57 @@
 
 import NavBar from '../../_components/navBar.tsx';
 import {
-  ProductImage,
-  ProductInfo,
+    ProductImage,
+    ProductInfo,
 } from '../_components/productIdComponents.tsx';
 import { cookies } from 'next/headers';
 import {
-  fetchStockListingWithId,
-  fetchStockVariantsWithListingId,
+    fetchStockListingWithId,
+    fetchStockVariantsWithListingId,
 } from '../../serverActions/viewActions.tsx';
-import prisma from '../../serverActions/db.ts';
 
 export default async function Page({
-  params,
+    params,
 }: {
-  params: { productId: number };
+    params: { productId: string };
 }) {
-  let cookieId = '';
-  let cookieList = cookies();
-  if (cookieList.has('id')) {
-    cookieId = cookies().get('id')!.value;
-  }
-  let stockVariants = await fetchStockVariantsWithListingId(params.productId);
-  let stockListing = await fetchStockListingWithId(params.productId);
-  if (stockListing == null) {
-    return (
-      <div className='flex justify-center items-center w-full h-screen mainTitle bg-black'>
-        <h1 className='text-white'> 404 - Product not found!</h1>
-      </div>
+    let cookieId = '';
+    let cookieList = cookies();
+    if (cookieList.has('id')) {
+        cookieId = cookies().get('id')!.value;
+    }
+    let stockVariants = await fetchStockVariantsWithListingId(
+        parseInt(params.productId),
     );
-  } else {
-    return (
-      <div className='h-screen w-screen flex flex-col items-center min-w-[100px]'>
-        <NavBar activePath={'/shop'} />
-        <div className='flex max-w-[1200px] pt-5'>
-          <main className='flex w-full'>
-            <div className='flex flex-col w-full pt-5 sm:flex-row sm:items-start'>
-              <ProductImage
-                productId={params.productId}
-                pictureCount={stockListing.pictureCount}
-              />
-              <ProductInfo
-                productName={stockListing.name}
-                productDescription={stockListing.description.split(';')}
-                cookieId={cookieId}
-                variantList={stockVariants}
-                productId={params.productId}
-              />
+    let stockListing = await fetchStockListingWithId(parseInt(params.productId));
+    if (stockListing == null) {
+        return (
+            <div className='flex justify-center items-center w-full h-screen mainTitle bg-black'>
+                <h1 className='text-white'> 404 - Product not found!</h1>
             </div>
-          </main>
-        </div>
-      </div>
-    );
-  }
+        );
+    } else {
+        return (
+            <div className='h-screen w-screen flex flex-col items-center min-w-[100px]'>
+                <NavBar activePath={'/shop'} />
+                <div className='flex max-w-[1200px] pt-5'>
+                    <main className='flex w-full'>
+                        <div className='flex flex-col w-full pt-5 sm:flex-row sm:items-start'>
+                            <ProductImage
+                                productId={parseInt(params.productId)}
+                                pictureCount={stockListing.pictureCount}
+                            />
+                            <ProductInfo
+                                productName={stockListing.name}
+                                productDescription={stockListing.description.split(';')}
+                                cookieId={cookieId}
+                                variantList={stockVariants}
+                                productId={parseInt(params.productId)}
+                            />
+                        </div>
+                    </main>
+                </div>
+            </div>
+        );
+    }
 }

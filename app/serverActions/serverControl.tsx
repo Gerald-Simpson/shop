24ee509f9f6294a18bafd68c700f5878e5f8bbd9
Gerry <gtnsimpson@gmail.com>
2024-11-20 11:47:48 +0000

    Updated package.json to allow for client rebuild.

--- app/serverActions/serverControl.tsx
+++ app/serverActions/serverControl.tsx
@@ -2,12 +2,17 @@
 import { exec } from 'child_process';
 
 export async function nextRebuild() {
-    console.log('nextRebuild');
     if (process.env.TEST_ENV === 'true') {
-        console.log('does nothing as test environment');
+        console.log('test env rebuild');
+        //exec('bash rebuildBash.sh', function(err, stdout, stderr) {
+        exec('npm run next-rebuild-dev', function(err, stdout, stderr) {
+            if (err) console.error(stderr);
+            console.log(stdout);
+        });
     } else if (process.env.TEST_ENV === 'false') {
         exec('npm run next-rebuild', function(err, stdout, stderr) {
             if (err) console.error(stderr);
+            console.log(stdout);
         });
     }
 }
